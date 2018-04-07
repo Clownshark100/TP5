faf4a2adf1682635d7f36347aa0ce06c1f32d26b
Daniel Nahum <danah@polymtl.ca>
2018-04-07 19:16:32 -0400

    Tout compile, ObtenirProduitSuivant pas implementer, crash a runtime

--- GestionnaireUsagers.cpp
+++ GestionnaireUsagers.cpp
@@ -5,3 +5,27 @@
 *******************************************/
 
 #include "GestionnaireUsagers.h"
+double GestionnaireUsagers::obtenirChiffreAffaires() const {
+	double chiffreAffaires = 0;
+	for (auto it = conteneur_.begin(); it != conteneur_.end(); it++) {
+		chiffreAffaires += (*it)->obtenirTotalAPayer();
+	}
+	return chiffreAffaires;
+}
+void GestionnaireUsagers::encherir(Client* client, ProduitAuxEncheres *produit, double montant) const {
+	if (produit->obtenirPrix() < montant)
+		produit->mettreAJourEnchere(client, montant);
+}
+
+void GestionnaireUsagers::reinitialiser() {
+	for (auto it = conteneur_.begin(); it != conteneur_.end(); it++) {
+		(*it)->reinitialiser();
+	}
+}
+
+void GestionnaireUsagers::afficherProfils() const {
+	for (auto it = conteneur_.begin(); it != conteneur_.end(); it++) {
+		(*it)->afficher();
+	}
+	cout << endl;
+}
